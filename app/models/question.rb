4a5234d4711c8be4cd0be0e25aa5d8bcdd031785
EC2 Default User <ec2-user@ip-172-31-45-186.ap-northeast-1.compute.internal>
2020-03-29 14:20:16 +0000

    [Add] Validation for User/Question and Layout for Question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,7 @@
 class Question < ApplicationRecord
+    validates :subject, presence: true
+    validates :content, presence: true
+	validates :subject, length: { maximum: 50 }
+	validates :content, length: { maximum: 1000 }
     belongs_to :user
 end
